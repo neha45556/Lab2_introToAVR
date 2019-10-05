fe259d01f8534951cb8b80fd7e043097dfec611f
neha45556 <neha45556@gmail.com>
2019-10-04 18:40:52 -0700

    Update ngupt009_lab2_part2.c

--- Lab2_introToAVR/turnin/ngupt009_lab2_part2.c
+++ Lab2_introToAVR/turnin/ngupt009_lab2_part2.c
@@ -1,63 +1,54 @@
-# Test file for Lab2_introToAVR
+/*	Author: ngupt009
+ *  Partner(s) Name: Surya Singh
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
 
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    
+    unsigned char tmpA = 0x00;
+    unsigned char tmpB = 0x00;
+    unsigned char tmpC = 0x00;
+    unsigned char tmpD = 0x00;
+    
+    unsigned char cntavail = 4;
+    
 
-# commands.gdb provides the following functions for ease:
-#   test "<message>"
-#       Where <message> is the message to print. Must call this at the beginning of every test
-#       Example: test "PINA: 0x00 => expect PORTC: 0x01"
-#   checkResult
-#       Verify if the test passed or failed. Prints "passed." or "failed." accordingly, 
-#       Must call this at the end of every test.
-#   expectPORTx <val>
-#       With x as the port (A,B,C,D)
-#       The value the port is epected to have. If not it will print the erroneous actual value
-#   setPINx <val>
-#       With x as the port or pin (A,B,C,D)
-#       The value to set the pin to (can be decimal or hexidecimal
-#       Example: setPINA 0x01
-#   printPORTx f OR printPINx f 
-#       With x as the port or pin (A,B,C,D)
-#       With f as a format option which can be: [d] decimal, [x] hexadecmial (default), [t] binary 
-#       Example: printPORTC d
-#   printDDRx
-#       With x as the DDR (A,B,C,D)
-#       Example: printDDRB
-
-echo ======================================================\n
-echo Running all tests..."\n\n
-
-test “PINA: 0x00 => PORTC: 0x04”
-setPINA 0x00
-continue 5
-expectPORTC 0x04
-checkResult
-
-test “PINA: 0x01 => PORTC: 0x03”
-setPINA 0x01
-continue 5
-expectPORTC 0x03
-checkResult
-
-test “PINA: 0x04 => PORTC: 0x03”
-setPINA 0x04
-continue 5
-expectPORTC 0x03
-checkResult
-
-test “PINA: 0x08 => PORTC: 0x03”
-setPINA 0x08
-continue 5
-expectPORTC 0x03
-checkResult
-
-
-
-
-
-
-# Add tests below
-
-# Report on how many tests passed/tests ran
-set $passed=$tests-$failed
-eval "shell echo Passed %d/%d tests.\n",$passed,$tests
-echo ======================================================\n
+   
+    /* Insert your solution below */
+    while (1) {
+        tmpA = PINA & 0x01;
+        tmpB = PINA & 0x02;
+        tmpC = PINA & 0x04;
+        tmpD = PINA & 0x08;
+        
+        if(tmpA == 0x01){
+            --cntavail;
+        }
+        if(tmpB == 0x02){
+            --cntavail;
+        }
+        if(tmpC == 0x04){
+            --cntavail;
+        }
+        if(tmpD == 0x08){
+            --cntavail;
+        }
+        
+        
+        PORTC = cntavail;
+        cntavail = 4 ;
+    }
+    return 1;
+}
