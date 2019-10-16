c1c0612fad8b006dc0f85cbde9ff65eb239b1b85
neha45556 <neha45556@gmail.com>
2019-10-16 10:03:50 -0700

    Update main.c

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -14,44 +14,8 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0xFF; PORTB = 0x00;
-    
-    unsigned char tmpA = 0x00;
-    unsigned char tmpB = 0x00;
-    unsigned char tmpC = 0x00;
-    unsigned char tmpD = 0x00;
-    
-    unsigned char cntavail = 4;
-    
-
-   
-    /* Insert your solution below */
-    while (1) {
-        tmpA = PINA & 0x01;
-        tmpB = PINA & 0x02;
-        tmpC = PINA & 0x04;
-        tmpD = PINA & 0x08;
-        
-        if(tmpA == 0x01){
-            --cntavail;
-        }
-        if(tmpB == 0x02){
-            --cntavail;
-        }
-        if(tmpC == 0x04){
-            --cntavail;
-        }
-        if(tmpD == 0x08){
-            --cntavail;
-        }
-          
-        if(cntavail == 0){
-            cntavail = 0x80;
-        }
-        
-        PORTC = cntavail;
-        cntavail = 4 ;
-    }
-    return 1;
+   DDRB = 0xFF; PORTB = 0x00;
+   while(1){
+       PORTB = 0x0F;
+   }
 }
